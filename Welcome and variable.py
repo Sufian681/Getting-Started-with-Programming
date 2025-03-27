9bd8b5d54ba68dfc45f8c49f955c30fd489dda8a
Sufi <sufiansonic@gmail.com>
2025-03-27 19:45:49 +0900

    first commit

---
+++ Welcome and variable.py
@@ -0,0 +1,9 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+height = input("Enter your height in cm:")
+
+print("Hello World \nMy name is",name,"\nand My age is ", age, "\nMy Height is ", height,"cm")
+
+print()
+
+print(f"Hello World \nMy name is {name} \nand My age is {age} \nMy Height is {height} cm")
