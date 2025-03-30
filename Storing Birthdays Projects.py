678d6078ddca125b4781e3a88d697e627e04b961
Sufi <sufiansonic@gmail.com>
2025-03-30 12:13:54 +0900

    duiw

---
+++ Storing Birthdays Projects.py
@@ -0,0 +1,13 @@
+friend1 = input("What is your name[1st] : ")
+bday1 = input("What is your Birthday [1st] : ")
+
+friend2 = input("What is your name[2nd] : ")
+bday2 = input("What is your Birthday [2nd] : ")
+
+friend3 = input("What is your name[3rd] : ")
+bday3 = input("What is your Birthday [3rd] : ")
+
+print(f"My 1st friends name is {friend1} and his Birthday is on {bday1}")
+print(f"My 2nd friends name is {friend2} and his Birthday is on {bday2}")
+print(f"My 3rd friends name is {friend3} and his Birthday is on {bday3}")
+
